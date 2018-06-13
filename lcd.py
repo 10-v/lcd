7fed52ac3f17405f5a69234dd9c53eed8a708d07
10v <39022669+10-v@users.noreply.github.com>
2018-06-12 20:47:41 -0500

    Included the missing try block

--- lcd.py
+++ lcd.py
@@ -8,20 +8,17 @@ import lcddriver
 disp = lcddriver.lcd()
 
 #Begin the while true block, this will run the program indefinitely till a key is pressed
-while True:
-    print("Printing message to LCD Display") # Display message on your terminal
-    disp.lcd_display_string("What's up?",1) # Display message on your LCD on Line #1
-    disp.lcd_display_string("10v's Tutorials",2) # Display message on your LCD on Line #2
-    time.sleep(3) # Delay of 3 seconds
-    disp.lcd_display_string("Happy Pi Programming",1) # Display message on your LCD on Line #1
-    time.sleep(3) # Delay of 3 seconds
-    disp.lcd_clear() # Clear the LCD screen
-    time.sleep(3) # Delay of 3 seconds
+try:
+    while True:
+        print("Printing message to LCD Display") # Display message on your terminal
+        disp.lcd_display_string("What's up?",1) # Display message on your LCD on Line #1
+        disp.lcd_display_string("10v's Tutorials",2) # Display message on your LCD on Line #2
+        time.sleep(3) # Delay of 3 seconds
+        disp.lcd_display_string("Happy Pi Programming",1) # Display message on your LCD on Line #1
+        time.sleep(3) # Delay of 3 seconds
+        disp.lcd_clear() # Clear the LCD screen
+        time.sleep(3) # Delay of 3 seconds
+
 except KeyboardInterrupt: # When any key is pressed, it interrupts the program
     print("Bye Bye!")  #Display departing message on terminal
-    disp.lcd_clear () #Make sure you clear the display screen when program is exiting. 
-        
-    
-
-
-
+    disp.lcd_clear () #Make sure you clear the display screen when program is exiting.
